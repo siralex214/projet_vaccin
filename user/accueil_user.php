51d4eca89c4afbd254be4e7be16db5d6bf092d17
alexis Mouchon <alexismouchon09@gmail.com>
2022-01-11 17:30:28 +0100

    modification user, suppression du compte, page user finis plus qu'a faire le responsive

--- user/accueil_user.php
+++ user/accueil_user.php
@@ -3,7 +3,6 @@ require_once "../inclu/pdo.php";
 require_once "../inclu/function.php";
 
 
-
 if (empty($_SESSION['connecter'])) {
     header("location: ../index.php");
     die();
@@ -16,7 +15,6 @@ if ($_SESSION['id'] != $_GET['id']) {
     header("location: ./accueil_user.php?id=" . $_SESSION['id']);
     die();
 }
-
 
 
 $info_user = $pdo->prepare("SELECT * FROM users where id =" . $_SESSION['id']);
@@ -54,7 +52,7 @@ if (!empty($_POST['submitted'])) {
         $query->bindValue(':type_vaccin', $_POST['type_vaccin'], PDO::PARAM_STR);
         $query->bindValue(':nom_vaccin', $_POST['nom_vaccin'], PDO::PARAM_STR);
         $query->bindValue(':date_injection', $_POST['date_injection']);
-        $query->bindValue(":id_user",$id_user,PDO::PARAM_STR);
+        $query->bindValue(":id_user", $id_user, PDO::PARAM_STR);
         $query->execute();
         header("location: show_all_vaccin.php");
     }
@@ -86,6 +84,10 @@ if (!empty($_POST['submitted'])) {
             <button id="button1">Accueil</button>
             <button id="button2">Mes informations</button>
             <button id="button3">Ajouter un Vaccin</button>
+            <div>
+                <button id="button4"><a style="color: black" href="./delete_user.php">Supprimer mon compte</a></button>
+            </div>
+
         </div>
         <p id="coucou"></p>
         <div>
@@ -99,7 +101,7 @@ if (!empty($_POST['submitted'])) {
                         echo " " . ucfirst($info_user['nom']) ?></h2>
                     <h3 class="titre_3">Mes derniers vaccins</h3>
                     <ul>
-                        <?php if (!$last_vaccin){ ?>
+                        <?php if (!$last_vaccin) { ?>
                             <p style="color: white">Aucun vaccin enregistré</p>
                         <?php } ?>
                         <?php foreach ($last_vaccin as $vaccin) :
@@ -114,9 +116,9 @@ if (!empty($_POST['submitted'])) {
                 <div class="prochain_vaccins">
                     <h3 class="titre_3">Mes prochaines vaccinations:</h3>
                     <ul>
-                        <?php if (!$last_vaccin){ ?>
+                        <?php if (!$last_vaccin) { ?>
                             <p style="color: white">Aucun vaccin enregistré</p>
-                       <?php } ?>
+                        <?php } ?>
                         <?php foreach ($last_vaccin as $vaccin) :
                             $date_injection = new DateTime($vaccin['date_injection']);
                             $date_injection->add(new DateInterval('P10M'));               //Où 'P10M' indique 'Période de 10 Mois'
@@ -132,7 +134,7 @@ if (!empty($_POST['submitted'])) {
                                 $corp = "Bonjour,
                                 Ceci est un mail automatique, merci de ne pas répondre.
                                 
-                                    Vous êtes éligible pour effectuer une nouvelle dose de vaccin contre: ". $vaccin['type_vaccin']."
+                                    Vous êtes éligible pour effectuer une nouvelle dose de vaccin contre: " . $vaccin['type_vaccin'] . "
                                     Rendez vous sur: http://localhost/projet_vaccin/index.php pour plus d'information.";
                                 $headers = "From: sosvaccin@gmail.com";
 //                                if (mail($dest, $sujet, $corp, $headers)) {
@@ -157,7 +159,8 @@ if (!empty($_POST['submitted'])) {
                     <p>Age: <?= $date_naissance ?> (<?= $age ?> ans)</p>
                     <p>Email: <?= $info_user['email'] ?></p>
                     <p style="font-size:1rem ;margin-top: 0.5rem"><a
-                                style="color: blue; text-decoration: underline black" href="./update_info_user.php">Modifier mes informations
+                                style="color: blue; text-decoration: underline black" href="./update_info_user.php">Modifier
+                            mes informations
                             personnelles.</a></p>
                 </div>
             </div>
@@ -167,62 +170,113 @@ if (!empty($_POST['submitted'])) {
                         <div class="form_input">
                             <label for="type_vaccin">Type du vaccin</label>
                             <select name="type_vaccin" id="type_vaccin">
-                                <option <?php if ($vaccin['type_vaccin'] == "la diphtérie") {echo "selected";} ?> value="la diphtérie">la diphtérie</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "le tétanos") {echo "selected";} ?> value="le tétanos">le tétanos</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "la poliomyélite") {echo "selected";} ?> value="la poliomyélite">la poliomyélite</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "COVID 19") {echo "selected";} ?> value="COVID 19">COVID 19</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "coqueluche") {echo "selected";} ?> value="coqueluche">coqueluche</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Fièvre jaune") {echo "selected";} ?> value="Fièvre jaune">Fièvre jaune</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Fièvre typhoïde") {echo "selected";} ?> value="Fièvre typhoïde">Fièvre typhoïde</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Grippe (influenza)") {echo "selected";} ?> value="Grippe (influenza)">Grippe (influenza)</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Haemophilus influenzae b") {echo "selected";} ?> value="Haemophilus influenzae b">Haemophilus influenzae b</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Hépatite A") {echo "selected";} ?> value="Hépatite A">Hépatite A</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Hépatite B") {echo "selected";} ?> value="Hépatite B">Hépatite B</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "HPV – virus du papillome humain") {echo "selected";} ?> value="HPV – virus du papillome humain">HPV – virus du papillome humain</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Méningo-encéphalite à tiques") {echo "selected";} ?> value="Méningo-encéphalite à tiques">Méningo-encéphalite à tiques</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Méningocoques") {echo "selected";} ?> value="Méningocoques">Méningocoques</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Oreillons") {echo "selected";} ?> value="Oreillons">Oreillons</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Pneumocoques") {echo "selected";} ?> value="Pneumocoques">Pneumocoques</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Poliomyélite") {echo "selected";} ?> value="Poliomyélite">Poliomyélite</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Rage") {echo "selected";} ?> value="Rage">Rage</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Rotavirus") {echo "selected";} ?> value="Rotavirus">Rotavirus</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Rougeole") {echo "selected";} ?> value="Rougeole">Rougeole</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Rubéole") {echo "selected";} ?> value="Rubéole">Rubéole</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Tuberculose") {echo "selected";} ?> value="Tuberculose">Tuberculose</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Varicelle") {echo "selected";} ?> value="Varicelle">Varicelle</option>
-                                <option <?php if ($vaccin['type_vaccin'] == "Zona (herpès zoster)") {echo "selected";} ?> value="Zona (herpès zoster)">Zona (herpès zoster)</option>
+                                <option value="la diphtérie">la diphtérie
+                                </option>
+                                <option value="le tétanos">le tétanos
+                                </option>
+                                <option value="la poliomyélite">la poliomyélite
+                                </option>
+                                <option value="COVID 19">COVID 19
+                                </option>
+                                <option value="coqueluche">coqueluche
+                                </option>
+                                <option value="Fièvre jaune">Fièvre jaune
+                                </option>
+                                <option value="Fièvre typhoïde">Fièvre typhoïde
+                                </option>
+                                <option  value="Grippe (influenza)">Grippe (influenza)
+                                </option>
+                                <option  value="Haemophilus influenzae b">Haemophilus influenzae b
+                                </option>
+                                <option  value="Hépatite A">Hépatite A
+                                </option>
+                                <option value="Hépatite B">Hépatite B
+                                </option>
+                                <option value="HPV – virus du papillome humain">HPV – virus du papillome humain
+                                </option>
+                                <option value="Méningo-encéphalite à tiques">Méningo-encéphalite à tiques
+                                </option>
+                                <option value="Méningocoques">Méningocoques
+                                </option>
+                                <option value="Oreillons">Oreillons
+                                </option>
+                                <option value="Pneumocoques">Pneumocoques
+                                </option>
+                                <option value="Poliomyélite">Poliomyélite
+                                </option>
+                                <option value="Rage">Rage
+                                </option>
+                                <option value="Rotavirus">Rotavirus
+                                </option>
+                                <option value="Rougeole">Rougeole
+                                </option>
+                                <option value="Rubéole">Rubéole
+                                </option>
+                                <option value="Tuberculose">Tuberculose
+                                </option>
+                                <option value="Varicelle">Varicelle
+                                </option>
+                                <option value="Zona (herpès zoster)">Zona (herpès zoster)
+                                </option>
                             </select>
                         </div>
                         <div class="form_input">
                             <label for="nom_vaccin">Nom du vaccin</label>
                             <select name="nom_vaccin" id="nom_vaccin">
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Boostrixtetra®"){echo "selected";} ?> value="Boostrixtetra®">Boostrixtetra®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Pfizer"){echo "selected";} ?> value="Pfizer">Pfizer</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Moderna"){echo "selected";} ?> value="Moderna">Moderna</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Johnson & Johnson"){echo "selected";} ?> value="Johnson & Johnson">Johnson & Johnson</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Havrix 1440®"){echo "selected";} ?> value="Havrix 1440®">Havrix 1440®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Havrix 720®"){echo "selected";} ?> value="Havrix 720®">Havrix 720®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Efluelda®"){echo "selected";} ?> value="Efluelda®">Efluelda®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Act-Hib®"){echo "selected";} ?> value="Act-Hib®">Act-Hib®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Avaxim 160®"){echo "selected";} ?> value="Avaxim 160®">Avaxim 160®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Avaxim 80®"){echo "selected";} ?> value="Avaxim 80®">Avaxim 80®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Engerix B 10®"){echo "selected";} ?> value="Engerix B 10®">Engerix B 10®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Engerix B 20®"){echo "selected";} ?> value="Engerix B 20®">Engerix B 20®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Engerix B 20®"){echo "selected";} ?> value="HBVAXPRO 10®">Engerix B 20®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Engerix B 20®"){echo "selected";} ?> value="HBVAXPRO 5®">Engerix B 20®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Cervarix®"){echo "selected";} ?> value="Cervarix®">Cervarix®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Gardasil 9®"){echo "selected";} ?> value="Gardasil 9®">Gardasil 9®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Encepur 0,5 ml®"){echo "selected";} ?> value="Encepur 0,5 ml®">Encepur 0,5 ml®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Hexyon®"){echo "selected";} ?> value="Hexyon®">Hexyon®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Imovax Polio®"){echo "selected";} ?> value="Imovax Polio®">Imovax Polio®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "M-M-RVaxpro®"){echo "selected";} ?> value="M-M-RVaxpro®">M-M-RVaxpro®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Pentavac®"){echo "selected";} ?> value="Pentavac®">Pentavac®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Rabipur®"){echo "selected";} ?> value="Rabipur®">Rabipur®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Rotarix®"){echo "selected";} ?> value="Rotarix®">Rotarix®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Rotateq®"){echo "selected";} ?> value="Rotateq®">Rotateq®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Stamaril®"){echo "selected";} ?> value="Stamaril®">Stamaril®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Varilrix®"){echo "selected";} ?> value="Varilrix®">Varilrix®</option>
-                                <option <?php if ($vaccin['nom_du_vaccin'] == "Zostavax®"){echo "selected";} ?> value="Zostavax®">Zostavax®</option>
+                                <option value="Boostrixtetra®">Boostrixtetra®
+                                </option>
+                                <option value="Pfizer">Pfizer
+                                </option>
+                                <option value="Moderna">Moderna
+                                </option>
+                                <option value="Johnson & Johnson">Johnson & Johnson
+                                </option>
+                                <option value="Havrix 1440®">Havrix 1440®
+                                </option>
+                                <option value="Havrix 720®">Havrix 720®
+                                </option>
+                                <option value="Efluelda®">Efluelda®
+                                </option>
+                                <option value="Act-Hib®">Act-Hib®
+                                </option>
+                                <option value="Avaxim 160®">Avaxim 160®
+                                </option>
+                                <option value="Avaxim 80®">Avaxim 80®
+                                </option>
+                                <option value="Engerix B 10®">Engerix B 10®
+                                </option>
+                                <option value="Engerix B 20®">Engerix B 20®
+                                </option>
+                                <option value="HBVAXPRO 10®">Engerix B 20®
+                                </option>
+                                <option value="HBVAXPRO 5®">Engerix B 20®
+                                </option>
+                                <option value="Cervarix®">Cervarix®
+                                </option>
+                                <option value="Gardasil 9®">Gardasil 9®
+                                </option>
+                                <option value="Encepur 0,5 ml®">Encepur 0,5 ml®
+                                </option>
+                                <option value="Hexyon®">Hexyon®
+                                </option>
+                                <option value="Imovax Polio®">Imovax Polio®
+                                </option>
+                                <option value="M-M-RVaxpro®">M-M-RVaxpro®
+                                </option>
+                                <option value="Pentavac®">Pentavac®
+                                </option>
+                                <option value="Rabipur®">Rabipur®
+                                </option>
+                                <option value="Rotarix®">Rotarix®
+                                </option>
+                                <option value="Rotateq®">Rotateq®
+                                </option>
+                                <option value="Stamaril®">Stamaril®
+                                </option>
+                                <option value="Varilrix®">Varilrix®
+                                </option>
+                                <option value="Zostavax®">Zostavax®
+                                </option>
                             </select>
                         </div>
                         <div class="form_input">
@@ -230,7 +284,8 @@ if (!empty($_POST['submitted'])) {
                             <input type="datetime-local" name="date_injection" id="date_injection">
                         </div>
                         <div class="form_input">
-                            <input class="input_submit submit_registration" type="submit" value="Ajouter" name="submitted">
+                            <input class="input_submit submit_registration" type="submit" value="Ajouter"
+                                   name="submitted">
                         </div>
                     </form>
             </div>
